ebd0c18bcca0a7cef032afda075d1b3112b18219
sometowngeek <11671755+sometowngeek@users.noreply.github.com>
2022-03-20 09:55:01 -0500

    refactor: Add a better builder

--- src/main/java/org/geekcodes/calendar/FMonth.java
+++ src/main/java/org/geekcodes/calendar/FMonth.java
@@ -4,84 +4,144 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * The class FMonth.
  */
 public class FMonth {
-    /**
-     * The Local date.
-     */
-    private LocalDate localDate = null;
-    /**
-     * The Month.
-     */
-    private Month month = null;
-    /**
-     * The Year.
-     */
-    private int year = 0;
+    /** The Local date. */
+    private int day;
+
+    /** The Month. */
+    private Month month;
+
+    /** The Year. */
+    private int year;
 
     /**
-     * Instantiates a new F month.
+     * Instantiates a new FMonth.
      */
     private FMonth(){
         // Empty constructor
     }
 
-    /**
-     * Get instance f month.
-     *
-     * @return the f month
-     */
-    public static FMonth getInstance(){
-        return new FMonth();
+    public FMonth(FMonthBuilder fMonthBuilder) {
+        this.month = Month.of(fMonthBuilder.month);
+        this.year = fMonthBuilder.year;
+        this.day = fMonthBuilder.day;
+    }
+
+    public static FMonthBuilder builder(){
+        return new FMonthBuilder();
     }
 
     /**
-     * With month f month.
-     *
-     * @param month the month
-     * @return the f month
+     * The class F month builder.
      */
-    public FMonth withMonth(Month month){
-        this.month = month;
-        return this;
-    }
+    private static class FMonthBuilder {
+        private Integer year = null;
+        private Integer month = null;
+        private Integer day = null;
 
-    /**
-     * With year f month.
-     *
-     * @param year the year
-     * @return the f month
-     */
-    public FMonth withYear(int year){
-        this.year = year;
-        return this;
-    }
+        public FMonthBuilder() {}
 
-    /**
-     * Get local date local date.
-     *
-     * @return the local date
-     */
-    public LocalDate getLocalDate(){
-        List<String> errors = new ArrayList<>();
-
-        if (this.year == 0){
-            errors.add("Must call withYear() with a valid year.");
+        public FMonthBuilder withYear(int year){
+            this.year = year;
+            return this;
         }
 
-        if (this.month == null){
-            errors.add("Must call withMonth() with a valid month.");
+        public FMonthBuilder withMonth(int month){
+            this.month = month;
+            return this;
         }
 
-        if (errors.size() > 0){
-            throw new NullPointerException(String.join(" ", errors));
+        public FMonthBuilder withDay(int day){
+            this.day = day;
+            return this;
         }
 
-        return LocalDate.of(this.year, this.month, 1);
+        public FMonth build(){
+            this.validate();
+            return new FMonth(this);
+        }
+
+        /**
+         * Validate.
+         */
+        private void validate() {
+            List<String> messages = new ArrayList<>();
+
+            validateYear(messages);
+            validateMonth(messages);
+            validateDay(messages);
+
+            if (!messages.isEmpty()){
+                throw new IllegalStateException(messages.toString());
+            }
+        }
+
+        /**
+         * Validate year.
+         *
+         * @param messages the messages
+         */
+        private void validateYear(List<String> messages) {
+            if (this.year == null) {
+                messages.add("Year must not be null.");
+            }
+
+            if (this.year != null && this.year < 1970 ) {
+                messages.add("Year may not be before 1970.");
+            }
+        }
+
+        /**
+         * Validate month.
+         *
+         * @param messages the messages
+         */
+        private void validateMonth(List<String> messages) {
+            if (this.month == null) {
+                messages.add("Month must not be null.");
+            }
+
+            if (this.month != null && (this.month < 1 || this.month > 12)) {
+                messages.add("Month must be between 1 ad 12.");
+            }
+        }
+
+        /**
+         * Validate day.
+         *
+         * @param messages the messages
+         */
+        private void validateDay(List<String> messages){
+            int maxDays = 31;
+            if (this.day == null) {
+                messages.add("Day must not be null.");
+            }
+
+            if (this.month == null || this.year == null){
+                return;
+            }
+
+            if (this.month == 2) {
+                if (Math.floorMod(this.year, 4) == 0) {
+                    maxDays = 29;
+                }
+                maxDays = 28;
+            }
+
+            var days = Arrays.asList(31, maxDays, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+            int calculatedDays = days.get(this.month - 1).intValue();
+            int intDay = this.day.intValue();
+            if (intDay < 0 || intDay > calculatedDays) {
+                messages.add(String.format("Day must be between 1 and %d", calculatedDays));
+            }
+        }
     }
 
 }
